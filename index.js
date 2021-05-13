46b7bcab3708e24c4b25e7d236f9ed82793d6bed
awadbilal <awadbilal99@gmail.com>
2021-05-14 01:23:28 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((arr) => {
+    arr = arr.split(' ');
+    for(let i = 0; i < arr.length; i++){
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+      // console.log(arr[i]);
+    }
+    arr = arr.join(' ');
+    // console.log(arr);
+    return arr;
+  });
 }
+
+// titleCased();
